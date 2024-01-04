04f7805f927dd6be69b0b98c5924fe92a18b1222
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2024-01-04 06:10:49 +0000

    Deploying to gh-pages from @ MaltsevaYulia/keen-front-end@a4c1a1c7dab2a22cd0b3cab9075d8a1332ebb3e3 🚀

---
+++ static/js/281.5852c6a2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkeen_front_end=self.webpackChunkkeen_front_end||[]).push([[281],{281:function(e,n,r){r.r(n),r.d(n,{default:function(){return d}});r(791);var a="Header_header__OKWO7",t="Header_header__title__zG0D0",_=r(184),d=function(){return(0,_.jsx)("header",{className:a,children:(0,_.jsx)("h1",{className:t,children:"ADMIN.BIKE-BOOKING.COM"})})}}}]);
+//# sourceMappingURL=281.5852c6a2.chunk.js.map
